e17477db209d067cbe592e18d9a1aa64a2a6fc2a
sidiq1209 <71972668+sidiq1209@users.noreply.github.com>
2022-06-12 15:20:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas5_19030013/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.tugas5_19030013;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
